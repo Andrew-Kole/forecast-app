20a79e9416b530cf24f607c561b9af8e0e633f6f
pakitowka <whitesaint12@gmail.com>
2023-05-07 23:39:05 +0200

    forecast works

--- backend.py
+++ backend.py
@@ -6,20 +6,16 @@ import requests
 load_dotenv()
 API_KEY: typing.Final = os.getenv("API_KEY")
 
-def get_data(place, forecast_days, kind=None):
+def get_data(place, forecast_days):
     url = "http://api.openweathermap.org/data/2.5/forecast"
     r = requests.get(url, params={"q": place, "appid": API_KEY})
     data = r.json()
     filtered_data = data["list"]
     nr_values = forecast_days * 8
     filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo", forecast_days=3, kind="Sky"))
+    print(get_data(place="Tokyo", forecast_days=3))
