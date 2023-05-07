d16244c7ef8068abb9deac57eb7efe79f7dd7b71
pakitowka <whitesaint12@gmail.com>
2023-05-07 20:36:19 +0200

    data filtering

--- backend.py
+++ backend.py
@@ -2,18 +2,24 @@ import typing
 from dotenv import load_dotenv
 import os
 import requests
-import urllib
 
 load_dotenv()
 API_KEY: typing.Final = os.getenv("API_KEY")
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days, kind=None):
     url = "http://api.openweathermap.org/data/2.5/forecast"
     r = requests.get(url, params={"q": place, "appid": API_KEY})
     data = r.json()
-    return data
+    filtered_data = data["list"]
+    nr_values = forecast_days * 8
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
 
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", forecast_days=3, kind="Sky"))
